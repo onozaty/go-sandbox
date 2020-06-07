97bfb7c86f41d58dd1c80cf805fb5f72da912895
onozaty <onozaty@gmail.com>
2020-06-07 23:08:17 +0900

    [web-server] 引数でポートとディレクトリを指定できるように

--- web-server/main.go
+++ web-server/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./"))
+
+	var dir string
+	var port int
+
+	flag.StringVar(&dir, "d", "./", "dir")
+	flag.IntVar(&port, "p", 3000, "port")
+	flag.Parse()
+
+	fs := http.FileServer(http.Dir(dir))
 	http.Handle("/", fs)
 
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Printf("Listening...  dir=%s port=%d", dir, port)
+
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
